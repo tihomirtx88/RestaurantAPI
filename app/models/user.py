b0a5fbf2e5070ff488bca6b359ee94ac81b15c3e
tihomirtx88 <tihomirtx88@gmail.com>
2026-02-25 09:14:31 +0200

    Finish user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from app.extensions import db, bcrypt
 from datetime import datetime
 
-class User(db.model):
+class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +9,9 @@ class User(db.model):
     password = db.Column(db.String(255), nullable=False)
     is_verified = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, raw_password):
+        self.password = bcrypt.generate_password_hash(raw_password).decode("utf-8")
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password, raw_password)
