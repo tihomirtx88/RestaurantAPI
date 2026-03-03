b57ebd45c4453ab2703da5c9fd15c50719c02d49
tihomirtx88 <tihomirtx88@gmail.com>
2026-03-03 21:03:38 +0200

    Implement review model

---
+++ app/models/review.py
@@ -0,0 +1,17 @@
+from app.extensions import db
+from datetime import datetime
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    id = db.Column
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey("menu_items.id"), nullable=False)
