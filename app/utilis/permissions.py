b76d6ea1298f39076697b629e072256af62469ea
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-08 09:19:54 +0300

    RBAC middleware
    Implement RBAC middleware functionality

---
+++ app/utilis/permissions.py
@@ -0,0 +1,34 @@
+from flask_jwt_extended import get_jwt
+from functools import wraps
+from flask import jsonify
+
+def role_required(required_role):
+
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            # Taking data from token
+            jwt_data = get_jwt()
+
+            # Taking role of user
+            user_role = jwt_data.get("role")
+
+            # cheks
+            if user_role != required_role:
+                return jsonify({"message": "Forbidden"}), 403
+
+             # If everything is ok keep on to route
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator()
+
+
+# Use @route("/admin")
+# @jwt_required()
+# @role_required("admin")
+# def admin_panel():
+#     return {"message": "Welcome admin"}
