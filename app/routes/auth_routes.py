97c9ef067177b583ec3b8473bfdea72242b027a4
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-13 10:32:21 +0300

    Using custom error handler in auth route

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, jsonify
-from sqlalchemy import True_
+from flask import Blueprint, request
+from app.utilis.error_handler import AppError
 
 from app.extensions import db
 from app.models.token_blocklist import TokenBlocklist
@@ -13,10 +13,10 @@ def register():
     data = request.get_json()
 
     if not data or not data.get("email") or not data.get("password"):
-        return jsonify({"message": "Email and password required"}), 400
+        raise AppError("Email and password required", 400)
 
     if User.query.filter_by(email=data["email"]).first():
-        return jsonify({"message": "Email already exists"}), 400
+        raise AppError("Email already exists", 400)
 
     user = User(email=data["email"])
     user.set_password(data["password"])
@@ -24,19 +24,19 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"message": "User registered successfully"}), 201
+    return {"message": "User registered successfully"}, 201
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
 
     if not data.get("email") or not data.get("password"):
-      return jsonify({"message": "Email and password required"}), 400
+      raise AppError("Email and password required", 400)
 
     user = User.query.filter_by(email=data["email"]).first()
 
     if not user or not user.check_password(data["password"]):
-        return jsonify({"message": "Invalid credentials"}), 401
+        raise AppError("Invalid credentials", 401)
 
     access_token = create_access_token(
         identity=str(user.id),
@@ -44,11 +44,11 @@ def login():
     )
     refresh_token = create_refresh_token(identity=str(user.id))
 
-    return jsonify({
+    return {
         "access_token": access_token,
         "user_id": user.id,
         "refresh_token": refresh_token
-    }), 200
+    }, 200
 
 @auth_bp.route("/profile", methods=["GET"])
 @jwt_required()
@@ -56,10 +56,10 @@ def profile():
     user_id = get_jwt_identity()
     user = User.query.get(int(user_id))
 
-    return jsonify({
+    return {
         "email": user.email,
         "id": user.id
-    })
+    }, 200
 
 @auth_bp.route("/refresh", methods=["POST"])
 @jwt_required(refresh=True)
@@ -67,9 +67,9 @@ def refresh():
     user_id = get_jwt_identity()
     new_access_token = create_access_token(identity=user_id)
 
-    return jsonify({
+    return {
         "access_token": new_access_token
-    }), 200
+    }, 200
 
 @auth_bp.route("/logout", methods=["POST"])
 @jwt_required(refresh=True)
@@ -79,4 +79,4 @@ def logout():
     db.session.add(TokenBlocklist(jti=jti))
     db.session.commit()
 
-    return jsonify({"message": "Logged out"}), 200
+    return {"message": "Logged out"}, 200
