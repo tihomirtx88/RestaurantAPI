711df5dbe9728262dac391b1ede1babd1b59e098
tihomirtx88 <tihomirtx88@gmail.com>
2026-04-13 10:16:32 +0300

    Fixing Error Handler code

--- app/utilis/error_handler.py
+++ app/utilis/error_handler.py
@@ -3,12 +3,14 @@ from app.utilis.app_error import AppError
 
 def register_error_handlers(app):
 
+    #Custom error 
     @app.errorhandler(AppError)
     def handle_app_error(error):
         return jsonify({
-            register_error_handlers
+            "status": "fail",
+            "message": error.message
         }), error.status_code
-
+    # All other errors
     @app.errorhandler(Exception)
     def handle_generic_error(error):
         print("ERROR:", error)
