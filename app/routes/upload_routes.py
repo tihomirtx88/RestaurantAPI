149506a2b06a41d896ffeb44baf3cb785b84bada
tihomirtx88 <tihomirtx88@gmail.com>
2026-06-10 17:46:19 +0300

    Adding function for alowed files

--- app/routes/upload_routes.py
+++ app/routes/upload_routes.py
@@ -9,3 +9,18 @@ upload_bp = Blueprint(
     __name__,
     url_prefix="/api/upload"
 )
+
+ALLOWED_EXTENSIONS = {
+    "png",
+    "jpg",
+    "jpeg",
+    "webp"
+}
+
+def allowed_file(filename):
+
+    return (
+            "." in filename and
+            filename.rsplit(".", 1)[1].lower()
+            in ALLOWED_EXTENSIONS
+    )
